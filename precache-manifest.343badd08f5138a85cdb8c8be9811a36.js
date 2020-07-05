04e906ea3dc2e48d8ddba2a306ea0fe367dfb95a
Peter Liu <peter.ty.liu@gmail.com>
2020-07-04 23:36:44 -0400

    Updates

---
+++ precache-manifest.343badd08f5138a85cdb8c8be9811a36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d4b46f75fe361f6913b6f579df7be1",
+    "url": "/assignment-16-react-portfolio/index.html"
+  },
+  {
+    "revision": "6b91e39d99989a28c8e4",
+    "url": "/assignment-16-react-portfolio/static/css/main.9e8afb73.chunk.css"
+  },
+  {
+    "revision": "8a5b4b5c87ee3d69433b",
+    "url": "/assignment-16-react-portfolio/static/js/2.9c4d440c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/assignment-16-react-portfolio/static/js/2.9c4d440c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b91e39d99989a28c8e4",
+    "url": "/assignment-16-react-portfolio/static/js/main.ac26d0c7.chunk.js"
+  },
+  {
+    "revision": "0acad9e8d68aebc1aa99",
+    "url": "/assignment-16-react-portfolio/static/js/runtime-main.4458595f.js"
+  }
+]);
